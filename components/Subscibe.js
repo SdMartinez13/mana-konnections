83c877313efb48eb13465f6f7981abc338287435
Stephen Martinez <104386666+SdMartinez13@users.noreply.github.com>
2023-01-13 02:01:23 -0800

    links added to index, sidebar functionality

---
+++ components/Subscibe.js
@@ -0,0 +1,38 @@
+function Subscribe() {
+    return (
+<div className="">
+      <ul className="flex justify-center gap-2 m-4">
+        <li className="flex justify-center border border-black bg-red-300 w-full p-4 mb-4">
+          <a
+            target="_blank"
+            href="https://open.spotify.com/show/7tpFtwBh5BKcwrYa4V9iwf?si=3feee38f68344d70"
+            rel="noopener noreferrer"
+          >
+            Spotify
+          </a>
+        </li>
+        <li className="flex justify-center border border-black bg-red-300 w-full p-4 mb-4">
+          <a
+            target="_blank"
+            href="https://anchor.fm/sarah-blanche"
+            rel="noopener noreferrer"
+          >
+            Anchor.fm
+          </a>
+        </li>
+        <li className="flex justify-center border border-black bg-red-300 w-full p-4 mb-4">
+          <a
+            target="_blank"
+            href="https://www.instagram.com/dear_life_thank_you/"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+        </li>
+        
+      </ul>
+    </div>
+  );
+}
+
+export default Subscribe;
